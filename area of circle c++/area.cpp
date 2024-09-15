007462b517adf17e3735383a99b01e71c0c8a137
Nasir Ali <160453804+nasirali84@users.noreply.github.com>
2024-09-15 20:49:19 +0530

    Add files via upload

---
+++ area of circle c++/area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Calculate are of circle"<<endl;
+    int r;
+    cout<<"Enter the radius: ";
+    cin>>r;
+    float pi = 3.14;
+    cout<<"Area of circle is: "<<pi*r*r;
+}
